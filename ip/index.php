0e8d9c082f7738ee7d3ec75c1995bfae85efe0f2
Tiago Ilieve <tiago.myhro@gmail.com>
2014-07-20 16:16:37 -0300

    Fix remote IP detection behind reverse proxy

--- ip/index.php
+++ ip/index.php
@@ -1,5 +1,11 @@
 <?php
 
 header('Content-Type: text/plain; charset="UTF-8"');
+
+if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER))
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+else
+    $ip = $_SERVER['REMOTE_ADDR'];
+
 echo $_SERVER['HTTP_USER_AGENT'] . PHP_EOL . PHP_EOL;
-echo $_SERVER['REMOTE_ADDR'] . PHP_EOL;
+echo $ip . PHP_EOL;
