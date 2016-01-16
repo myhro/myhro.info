46363602331559dafb42514523442d914f36ae0d
Tiago Ilieve <tiago.myhro@gmail.com>
2016-01-16 10:33:39 -0200

    Fix IP detection behind multiple proxies

--- ip/index.php
+++ ip/index.php
@@ -2,10 +2,14 @@
 
 header('Content-Type: text/plain; charset="UTF-8"');
 
-if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER))
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-else
+if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+    $x_forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    $ips_list = explode(',', $x_forwarded_for);
+    $ip = array_shift($ips_list);
+}
+else {
     $ip = $_SERVER['REMOTE_ADDR'];
+}
 
 echo $_SERVER['HTTP_USER_AGENT'] . PHP_EOL . PHP_EOL;
 echo $ip . PHP_EOL;
