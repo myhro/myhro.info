67a8221dfcba2e76e726c9530f45c5518b6cb18a
Tiago Ilieve <tiago.myhro@gmail.com>
2020-03-21 15:03:11 -0300

    Drop 'malvados' worker
    Replaced for the actual deployment of https://github.com/myhro/malvados

--- workers/malvados/index.js
+++
@@ -1,8 +0,0 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-})
-
-async function handleRequest(request) {
-  let url = 'https://www.algolia.com/realtime-search-demo/malvados-ec87f464-c56f-419c-8987-8f1c9c9ce47f';
-  return Response.redirect(url, 302);
-}
