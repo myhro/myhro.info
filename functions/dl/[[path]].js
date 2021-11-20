f84fe87d54f3444821067456e91b56d5f44318fc
Tiago Ilieve <tiago.myhro@gmail.com>
2021-11-20 17:42:52 -0300

    functions: add 'dl'

---
+++ functions/dl/[[path]].js
@@ -0,0 +1,6 @@
+export async function onRequestGet({request}) {
+  let url = new URL(request.url);
+  let path = url.pathname.replace(/^\/dl/, "");
+  let target = "https://myhro-info-dl.s3.amazonaws.com" + path;
+  return fetch(target);
+}
