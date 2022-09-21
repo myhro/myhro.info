e68fd83cb2e8cd553c69386c6871201899df7933
Tiago Ilieve <tiago.myhro@gmail.com>
2022-09-21 10:36:18 -0300

    dl: migrate from S3 to R2

--- functions/dl/[[path]].js
+++ functions/dl/[[path]].js
@@ -1,6 +1,6 @@
 export async function onRequestGet({ request }) {
   const url = new URL(request.url);
   const path = url.pathname.replace(/^\/dl\//, '');
-  const target = `https://myhro-info-dl.s3.amazonaws.com/${path}`;
+  const target = `https://pub-5f2914f703904c419629da4d1ac2436f.r2.dev/${path}`;
   return fetch(target);
 }
