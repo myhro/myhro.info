7f447baf18e4f78ec2b20b945719b5fed253aa13
Tiago Ilieve <tiago.myhro@gmail.com>
2022-11-04 09:04:08 -0300

    dl: check if file exists before returning it
    The current default R2 '404 Not Found' page is a non-mobile-friendly
    HTML.

--- functions/dl/[[path]].js
+++ functions/dl/[[path]].js
@@ -2,5 +2,11 @@ export async function onRequestGet({ request }) {
   const url = new URL(request.url);
   const path = url.pathname.replace(/^\/dl\//, '');
   const target = `https://pub-5f2914f703904c419629da4d1ac2436f.r2.dev/${path}`;
+
+  const response = await fetch(target, { method: 'HEAD' });
+  if (response.status === 404) {
+    return new Response('Not Found', { status: 404 });
+  }
+
   return fetch(target);
 }
